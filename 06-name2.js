81b2459ac02ede15051248a6a102b077352c2714
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-07-10 21:38:26 +0700

    Update 06-name2.js

--- 06-name2.js
+++ 06-name2.js
@@ -1,4 +1,5 @@
-const createModules=require("./06-name1");
-
-const info=createModules.info;
-console.log(info);
+var fs=require("fs");
+fs.readFile("demoFile.txt","utf8",(err,data)=>{
+  if(err) throw err;
+  console.log(data)
+});
