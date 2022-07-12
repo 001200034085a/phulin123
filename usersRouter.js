5671c298fd7de78f6209f757acb1a800ceda9148
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-07-12 21:27:04 +0700

    Create usersRouter.js

---
+++ usersRouter.js
@@ -0,0 +1,39 @@
+const express=require("express");
+const usersRouter=express.Router();
+
+
+const users=[
+    {"id":1,"name":"đặng phú"}
+];
+// get
+usersRouter.get("/",(req,res)=>{
+    res.send(users)
+});
+// post
+usersRouter.post("/",(req,res)=>{
+    const newData={
+        id:users[users.length-1].id+1,
+        name:req.body.name
+    }
+    users.push(newData);
+    res.send(users)
+});
+// delete
+usersRouter.delete("/",(req,res)=>{
+    const newData1=[...users].filter(item=>{
+        return item.id !== req.body.id
+    })
+    res.send(newData1);
+})
+// put
+usersRouter.put("/",(req,res)=>{
+    const newData2=[...users];
+    for(let item of users){
+        if(item.id === req.body.id){
+            item.name=req.body.name
+        }
+    }
+    res.send(newData2)
+})
+
+module.exports=usersRouter;
