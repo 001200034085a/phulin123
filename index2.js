4a87737aff933d8a8eead835f618f35e096cc562
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-07-12 21:27:39 +0700

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,10 +1,12 @@
 const express=require("express");
 const app=express();
 const port=5000;
-const mangaRouter=require("./MangaRouter")
+const mangaRouter=require("./MangaRouter");
+const usersRouter=require("./UsersRouter");
 
 app.use(express.json());
-app.use("/api/manga",mangaRouter) 
+app.use("/api/manga",mangaRouter);
+app.use("/api/users",usersRouter); 
 
 app.listen(port,()=>{
     console.log("sever running http://localhost.5000")
