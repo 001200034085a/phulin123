2e47d66ca6138e21583061cd2900ffa376cb8e60
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-07-17 03:53:28 +0700

    Update usersRouter.js

--- usersRouter.js
+++ usersRouter.js
@@ -1,9 +1,10 @@
 const express=require("express");
+const joi=require("joi");
 const usersRouter=express.Router();
 
 
 const users=[
-    {"id":1,"name":"đặng phú"}
+    {"id":1,"name":"đặng phú","phoneNumber":"0234456789","email":"xcdj@gmail.com","gender":"nam","age":22}
 ];
 // get
 usersRouter.get("/",(req,res)=>{
@@ -11,12 +12,23 @@ usersRouter.get("/",(req,res)=>{
 });
 // post
 usersRouter.post("/",(req,res)=>{
-    const newData={
-        id:users[users.length-1].id+1,
-        name:req.body.name
+    const {error}=validateUsers(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message)
     }
-   users.push(newData);
-   res.send(users);
+    else{
+        const newData={
+             id:users[users.length-1].id+1,
+             name:req.body.name,
+             age:req.body.age,
+             phoneNumber:req.body.phoneNumber,
+             email:req.body.email,
+             gender:req.body.gender
+        }
+       users.push(newData);
+       res.send(users);
+    }
+    
 });
 // delete
 usersRouter.delete("/",(req,res)=>{
@@ -27,13 +39,52 @@ usersRouter.delete("/",(req,res)=>{
 })
 // put
 usersRouter.put("/",(req,res)=>{
-    const newData2=[...users];
+    const {error}=validateUsers(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message)
+    }
+    else{
+        const newData2=[...users];
     for(let item of users){
         if(item.id === req.body.id){
-            item.name=req.body.name
+            item.name=req.body.name,
+            item.age=req.body.age,
+            item.phoneNumber=req.body.phoneNumber,
+            item.email=req.body.email,
+            item.gender=req.body.gender
         }
-    }
+        }
     res.send(newData2)
-})
+    }
+        
+});
+// validate joi
+const validateUsers=(users)=>{
+    const schema=joi.object({
+        id:joi.number(),
+
+        name:joi.string()
+        .regex(/[A-Za-z]$/)
+        .min(5)
+        .required(),
+
+        email:joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net',"tk","js"] } }),
+
+        phoneNumber:joi.string()
+        .pattern(new RegExp('^[0-9]{10,12}$')),
+        
+        age:joi.number()
+        .integer()
+        .min(1)
+        .max(200),
+        
+        gender:joi.string()
+        .regex(/nam$|nữ$|khác$/)
+        
+        
+    });
+    return schema.validate(users);
+}
 
 module.exports=usersRouter;
