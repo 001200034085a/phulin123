18ba15d8bd3b95a9f886fdda6becdc285ad7e2a5
001200034085a <dangtienphu23@gmail.com>
2022-08-20 20:47:14 +0700

    add data

---
+++ hello-memes/src/data/CartData.js
@@ -0,0 +1,8 @@
+const CartData={
+    products:[
+        {id:1,name:"macbock",price:1400,image:"https://images.fpt.shop/unsafe/filters:quality(90)/fptshop.com.vn/uploads/images/tin-tuc/143178/Originals/MacBook-Pro-2020-cover.jpg"},
+        {id:2,name:"car",price:4000,image:"https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F0x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349"},
+        {id:3,name:"hotgirl",price:100000,image:"https://image-us.24h.com.vn/upload/4-2020/images/2020-12-04/anh-4-1607072674-201-width650height812.jpg"}
+    ]
+};
+export default CartData;
---
+++ hello-memes/src/data/ExpenseData.js
@@ -0,0 +1,12 @@
+const expenseData=[
+    {id:1,name:"expense1",date:"2022/03/06",amount:30},
+    {id:2,name:"expense2",date:"2022/06/07",amount:230},
+    {id:3,name:"expense3",date:"2021/05/08",amount:130},
+    {id:4,name:"expense4",date:"2020/07/06",amount:3000},
+    {id:5,name:"expense5",date:"2019/08/09",amount:390},
+    {id:6,name:"expense6",date:"2022/05/10",amount:350},
+    {id:7,name:"expense7",date:"2020/05/03",amount:130},
+    {id:8,name:"expense8",date:"2021/05/06",amount:320},
+    {id:9,name:"expense9",date:"2019/05/06",amount:1370},
+];
+export default expenseData;
